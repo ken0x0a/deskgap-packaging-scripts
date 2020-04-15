9c841095af399323bac6b8721ddd5b2d13af703b
Ken <2770219+ken0x0a@users.noreply.github.com>
2020-04-15 14:32:50 +0900

    style: lint fix

--- win-appx/appx.js
+++ win-appx/appx.js
@@ -1,12 +1,14 @@
+#!/usr/bin/env node
+
 // @ts-check
 
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { spawnSync } = require('child_process');
-const fse = require('fs-extra');
-const minimist = require('minimist');
-const path = require('path');
-const rcedit = require('rcedit');
+const { spawnSync } = require("child_process");
+const fse = require("fs-extra");
+const minimist = require("minimist");
+const path = require("path");
+const rcedit = require("rcedit");
 
 async function main() {
   const args = parseArgs();
@@ -20,25 +22,26 @@ async function main() {
     new Promise((resolve, reject) => {
       rcedit(...args, (error) => (error == null ? resolve() : reject(error)));
     });
-  const system = (command, args) => spawnSync('cmd', ['/c', command, ...args], { stdio: 'inherit' });
+  const system = (command, args) => spawnSync("cmd", ["/c", command, ...args], { stdio: "inherit" });
 
-  const deskgapPath = path.resolve(repoPath, 'node_modules\\deskgap\\dist\\DeskGap');
-  const appPath = path.resolve(repoPath, 'app');
+  const deskgapPath = path.resolve(repoPath, "node_modules\\deskgap\\dist\\DeskGap");
+  const appPath = path.resolve(repoPath, "app");
 
-  const workingFolder = path.resolve(repoPath, 'release\\build');
+  const workingFolder = path.resolve(repoPath, "release\\build");
 
-  const executableIcon = path.resolve(repoPath, 'release', args.icon);
-  const resources = path.resolve(repoPath, 'release\\resources');
-  const appxmanifest = path.resolve(repoPath, 'release\\appxmanifest.xml');
+  const executableIcon = path.resolve(repoPath, "release", args.icon);
+  const resources = path.resolve(repoPath, "release\\resources");
+  const appxmanifest = path.resolve(repoPath, "release\\appxmanifest.xml");
   // const appxmanifest = path.resolve(repoPath,"release\\appxmanifest_selfsigning.xml")
   const signingPFX = null; // path.resolve(repoPath,"release\\AppxTestRootAgency.pfx")
 
   let executableName = null;
   try {
-    const packageJSON = require(path.join(appPath, 'package.json'));
+    const packageJSON = require(path.join(appPath, "package.json"));
+    /** @type {string} */
     executableName = packageJSON.productName || packageJSON.name;
   } catch (e) {
-    console.error('\x1b[35m%s\x1b[0m', e);
+    console.error("\x1b[35m%s\x1b[0m", e);
     throw e;
     // process.exit(1);
   }
@@ -49,11 +52,11 @@ async function main() {
 
   await fse.copy(deskgapPath, executableName);
 
-  await fse.copy(resources, path.join(executableName, 'resources'));
-  await fse.copy(appxmanifest, path.join(executableName, 'appxmanifest.xml'));
+  await fse.copy(resources, path.join(executableName, "resources"));
+  await fse.copy(appxmanifest, path.join(executableName, "appxmanifest.xml"));
 
   {
-    const targetAppPath = path.join(executableName, 'resources', 'app');
+    const targetAppPath = path.join(executableName, "resources", "app");
     await fse.remove(targetAppPath);
     await fse.copy(appPath, targetAppPath);
   }
@@ -67,9 +70,9 @@ async function main() {
 
   {
     const appxFile = `${executableName}.appx`;
-    system('makeappx', ['pack', '/d', executableName, '/p', appxFile]);
+    system("makeappx", ["pack", "/d", executableName, "/p", appxFile]);
     if (signingPFX != null)
-      system('signtool', ['sign', '/fd', 'sha256', '/f', signingPFX, appxFile]);
+      system("signtool", ["sign", "/fd", "sha256", "/f", signingPFX, appxFile]);
   }
 }
 
@@ -78,13 +81,13 @@ function parseArgs() {
    * @type {import('minimist').ParsedArgs & import('./types').ArgsType}
    */
   const args = minimist(process.argv.slice(2), {
-    string: ['dirname', 'appName', 'icon'],
-    alias: { d: 'dirname', a: 'appName', i: 'icon' },
+    string: ["dirname", "appName", "icon"],
+    alias: { d: "dirname", a: "appName", i: "icon" },
     default: { dirname: process.cwd() },
-    '--': true,
+    "--": true,
     stopEarly: true /* populate _ with first non-option */,
     unknown(args, options) {
-      console.error('\x1b[35m%s\x1b[0m', 'Unknown option supplied:', _);
+      console.error("\x1b[35m%s\x1b[0m", "Unknown option supplied:", _);
       throw new Error(JSON.stringify({ args, options }, null, 2));
     } /* invoked on unknown param */,
   });
@@ -97,12 +100,12 @@ function parseArgs() {
     if (args[argName] === undefined) throw new Error(`"${argName}" is required!`);
   }
 
-  ensureArgExist('dirname');
-  ensureArgExist('icon');
+  ensureArgExist("dirname");
+  ensureArgExist("icon");
   return args;
 }
 
-process.on('unhandledRejection', (error) => {
+process.on("unhandledRejection", (error) => {
   throw error;
 });
 
